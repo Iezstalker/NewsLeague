10a2d5717e21bc7862d0a291c0977df5c8689778
Iezstalker <106069608+Iezstalker@users.noreply.github.com>
2023-09-16 22:51:22 +0530

    Converting class based components into function based components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description,imageUrl, newsUrl, author, date,source} = this.props;
+const NewsItem=(props)=>{
+    let { title, description,imageUrl, newsUrl, author, date,source} = props;
     return (
       <div className='container my-3'>
         <div className="card">
         <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1', left: '90%'}} >{source}</span>
-          <img src={!imageUrl? "https://images.hindustantimes.com/tech/img/2023/05/20/1600x900/exoplanet_1684604246137_1684604260516.png":imageUrl} className="card-img-top" alt="" />
+          <img src={imageUrl} className="card-img-top" alt="" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
@@ -17,7 +16,6 @@ export class NewsItem extends Component {
         </div>
       </div>
     )
-  }
 }
 
 export default NewsItem 
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import loading1 from './loading1.gif'
 
-export class Spinner extends Component {
-  render() {
+const Spinner =()=>{
     return (
       <div className='text-center'>
           <img src={loading1} alt="loading" />
       </div>
     )
   }
-}
 
 export default Spinner
